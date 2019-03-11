b0e6e1da364ee0b9900e8af9d71a7d7f568eff25
Sione Tenifa <johntenifa@yahoo.com>
2019-03-11 16:25:27 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import './App.css';
+import List from './List'
+import GroList from './GroList'
+
+class App extends Component {
+  
+  state = {
+    groLi: [
+      { id: 1, name: "Milk", complete: true, },
+      { id: 2, name: "Rice", complete: false, },
+      { id: 3, name: "Chicken", complete: false, },
+    ]
+  };
+
+  
+  // state = { groLi: [] }
+
+    getUniqId = () => {
+    //NOTE We are just using this as a helper function for id's since we aren't using a db yet
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+    }
+
+  addItem = (name) => {
+  const { groLi } = this.state;
+  const todo = { name, id: this.getUniqId() , complete: false }
+  this.setState({ groLi: [todo, ...groLi] }); 
+  }
+
+  handleClick = (id) => {
+    this.setState({
+      groLi: this.state.groLi.map( todo => {
+        if (todo.id === id) {
+          return { ...todo, complete: !todo.complete, }
+        }
+        return todo;
+      })
+    })
+  }
+
+  
+  
+  
+  render() {
+    const { groLi } = this.state;
+
+  
+
+    return (
+      <div className="App">
+        <ul>
+          <GroList addItem={this.addItem} />
+          <List name="Grocery List" items={groLi} todoClick={this.handleClick} />
+        </ul>
+
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+
+
+ // renderGroLi = () => {
+  //   const { groLi, } = this.state;
+  //   return groLi.map( groLi => 
+  //     <li key={groLi.id}>{groLi.name}</li>
+  //   )
+  // };
+  
